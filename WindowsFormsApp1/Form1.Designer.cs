7145259ceb6f21186cfc899d2448785d03eee654
Andrey <Andrey@DESKTOP-62UL83D>
2022-06-06 13:29:41 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.operantleft = new System.Windows.Forms.Label();
+            this.operantright = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.op = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // operantleft
+            // 
+            this.operantleft.AutoSize = true;
+            this.operantleft.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operantleft.ForeColor = System.Drawing.Color.White;
+            this.operantleft.Location = new System.Drawing.Point(26, 9);
+            this.operantleft.Name = "operantleft";
+            this.operantleft.Size = new System.Drawing.Size(111, 86);
+            this.operantleft.TabIndex = 0;
+            this.operantleft.Text = "88";
+            // 
+            // operantright
+            // 
+            this.operantright.AutoSize = true;
+            this.operantright.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.operantright.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operantright.ForeColor = System.Drawing.Color.White;
+            this.operantright.Location = new System.Drawing.Point(278, 9);
+            this.operantright.Name = "operantright";
+            this.operantright.Size = new System.Drawing.Size(111, 86);
+            this.operantright.TabIndex = 1;
+            this.operantright.Text = "66";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.progressBar1.Location = new System.Drawing.Point(23, 298);
+            this.progressBar1.Maximum = 140;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(659, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Value = 40;
+            // 
+            // op
+            // 
+            this.op.AutoSize = true;
+            this.op.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.op.ForeColor = System.Drawing.Color.White;
+            this.op.Location = new System.Drawing.Point(166, 6);
+            this.op.Name = "op";
+            this.op.Size = new System.Drawing.Size(82, 86);
+            this.op.TabIndex = 3;
+            this.op.Text = "+";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(413, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 86);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(530, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 93);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Menu;
+            this.button1.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(702, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 90);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "✅";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(98, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 86);
+            this.label2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(798, 378);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.op);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.operantright);
+            this.Controls.Add(this.operantleft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label operantleft;
+        private System.Windows.Forms.Label operantright;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label op;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int left, right, result;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateExpresion();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == result.ToString())
+            {
+                progressBar1.Value += 10;
+                GenerateExpresion();
+                textBox1.Text = "";
+            }
+            else
+            {
+                textBox1.Text = "";
+                progressBar1.Value -= 20;
+                GenerateExpresion();
+            }
+            switch (progressBar1.Value)
+            {
+                case (100):
+                    label2.Text = "Ты молодец!";
+                    break;
+                case (20):
+                    label2.Text = "Ты тупой!";
+                    break;
+                case(0):
+                    Application.Exit();
+                    break;
+                default:
+                    label2.Text = "";
+                    break;
+            }
+        }
+        public void GenerateExpresion()
+        {
+            left = random.Next(13, 200);
+            right = random.Next(13, 200);
+            result = left + right;
+            operantleft.Text = left.ToString();
+            operantright.Text = right.ToString();
+
+        }
+    }
+}
